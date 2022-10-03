7d356ad1cfcc23439c18ba2cb163b8f0f3bfb548
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-10-03 23:02:04 +0100

    reading forecast

--- weather.js
+++ weather.js
@@ -1,3 +1,6 @@
 fetch('http://api.weatherapi.com/v1/current.json?key=933ab32502784c9dacd213120220310&q=Kano&aqi=no')
 .then(response => response.json())
-.then(json => console.log(json));
+.then(json => 
+    console.log(`it's ${json.current.temp_c} celcious in kano today`)
+);
+
